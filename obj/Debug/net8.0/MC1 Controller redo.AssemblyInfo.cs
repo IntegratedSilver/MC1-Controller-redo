a994edfe2555f713a060fb37a72ed8e2a42586f8
Axeolotl <a.d.l.c.rubio@gmail.com>
2024-04-02 20:17:06 -0700

    push

--- obj/Debug/net8.0/MC1 Controller redo.AssemblyInfo.cs
+++ obj/Debug/net8.0/MC1 Controller redo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MC1 Controller redo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f2dd02b6656361f0366955f3b71c9c48f0d474")]
 [assembly: System.Reflection.AssemblyProductAttribute("MC1 Controller redo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MC1 Controller redo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
